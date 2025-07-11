852297d614df18a7b043050cd46e931acb6321aa
Yudai Udagawa <nimius.rd@gmail.com>
2025-07-11 02:41:40 +0000

    READMEのアイコンにタイトル属性を追加し、アクセシビリティを向上

--- .github/scripts/generate-readme.js
+++ .github/scripts/generate-readme.js
@@ -6,7 +6,7 @@ Handlebars.registerHelper('techIcon', function(icon, name) {
   // フルURLかどうかを判定
   const iconUrl = icon.startsWith('http') ? icon : `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${icon}`;
   return new Handlebars.SafeString(
-    `<img src="${iconUrl}" width="40" height="40" alt="${name}" />`
+    `<img src="${iconUrl}" width="40" height="40" alt="${name}" title="${name}" />`
   );
 });
 
